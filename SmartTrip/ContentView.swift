227324bcdeea90c8c2ec83a9bb56e9959d1f5375
Zichen Zhu <zichenz7@illinois.edu>
2026-05-27 17:37:15 +0800

    Initial Commit

---
+++ SmartTrip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartTrip
+//
+//  Created by 竺子宸 on 2026/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartTrip/SmartTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTripApp.swift
+//  SmartTrip
+//
+//  Created by 竺子宸 on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct SmartTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
